e2ec6680e81f048b1a3f2f8b02b6fd3dec8ed2df
kaushikn07 <kaushik.ncode@gmail.com>
2024-01-22 18:45:25 +0530

    fixed frontend and backend connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
 
   const handleRegister = async () => {
     try {
-      const response = await fetch('http://localhost:5000/register', {
+      const response = await fetch('http://localhost:5000/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
